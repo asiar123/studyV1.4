e07ab630239bc095fc14532aec798bd4c71fc48d
Michael Steban <edgar-bucheli@hotmail.com>
2023-07-15 14:22:01 -0500

    first commit 3

--- server/settings.py
+++ server/settings.py
@@ -81,19 +81,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
+import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db2036',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
